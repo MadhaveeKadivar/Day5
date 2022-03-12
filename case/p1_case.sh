71894c47556f27737ad81aab183d6343afa346f0
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-12 22:24:12 +0530

    Switch Case Problem - 1

---
+++ case/p1_case.sh
@@ -0,0 +1,38 @@
+echo "Enter only single digit number : "
+read no
+case $no in
+	0)
+		echo "Word : ZERO"
+		;;
+	1)
+		echo "Word : ONE"
+		;;
+	2)
+		echo "Word : TWO"
+		;;
+	3)
+		echo "Word : THREE"
+		;;
+	4)
+		echo "Word : FOUR"
+		;;
+	5)
+		echo "Word : FIVE"
+		;;
+	6)
+		echo "Word : SIX"
+		;;
+	7)
+		echo "Word : SEVEN"
+		;;
+	8)
+		echo "Word : EIGHT"
+		;;
+	9)
+		echo "Word : NINE"
+		;;
+	*)
+                echo "Enter correct option"
+                ;;
+esac
+
