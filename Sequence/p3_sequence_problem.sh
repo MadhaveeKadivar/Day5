835ff58e0da1a84a690adf49a796c8598a4eb50d
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-12 22:31:44 +0530

    Sequence Problem - 3

---
+++ Sequence/p3_sequence_problem.sh
@@ -0,0 +1,8 @@
+
+echo "****Add two Random Dice Number and Print the Result****"
+dice_no1=$(((RANDOM%6)+1))
+dice_no2=$(((RANDOM%6)+1))
+echo "First random dice number is : $dice_no1":
+echo "Second random dice number is : $dice_no2":
+echo "Sum of this two random dice numbers is : $(($dice_no1+$dice_no2))";
+
