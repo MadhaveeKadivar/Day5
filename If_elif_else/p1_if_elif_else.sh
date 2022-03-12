8d23c7fa25baf0160fa090eeea8db9bd6dee6835
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-12 22:29:00 +0530

    If elif else Problem - 1

---
+++ If_elif_else/p1_if_elif_else.sh
@@ -0,0 +1,25 @@
+echo "Enter only single digit number : "
+read no
+if(($no==0))
+then echo "$no in Word : ZERO"
+elif(($no==1))
+then echo "$no in Word : ONE"
+elif(($no==2))
+then echo "$no in Word : TWO"
+elif(($no==3))
+then echo "$no in Word : THREE"
+elif(($no==4))
+then echo "$no in Word : FOUR"
+elif(($no==5))
+then echo "$no in Word : FIVE"
+elif(($no==6))
+then echo "$no in Word : SIX"
+elif(($no==7))
+then echo "$no in Word : SEVEN"
+elif(($no==8))
+then echo "$no in Word : EIGHT"
+elif(($no==9))
+then echo "$no in Word : NINE"
+else
+echo "Please enter Only single digit number"
+fi
