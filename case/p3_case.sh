31b72b46b8e6aa44fc8d74ab983522f9973e2f8c
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-12 22:24:55 +0530

    Switch Case Problem - 3

---
+++ case/p3_case.sh
@@ -0,0 +1,32 @@
+echo "Enter number 1 or number should be power of 10 like 10,100,etc : "
+read no
+case $no in
+	1)
+		echo "UNIT"
+		;;
+	10)
+		echo "TEN"
+		;;
+	100)
+		echo "HUNDRED"
+		;;
+	1000)
+		echo "THOUSAND"
+		;;
+	10000)
+		echo "TEN THOUSAND"
+		;;
+	100000)
+		echo "HUNDRED THOUSAND"
+		;;
+	1000000)
+		echo "ONE MILLION"
+		;;
+	10000000)
+		echo "TEN MILIION"
+		;;
+	*)
+                echo "Enter correct option"
+                ;;
+esac
+
