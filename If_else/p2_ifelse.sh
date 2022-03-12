e64125a90b41699c4e50cc4f93c4808df77558f4
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-12 22:38:23 +0530

    Update p2_ifelse.sh

--- If_else/p2_ifelse.sh
+++ If_else/p2_ifelse.sh
@@ -1,3 +1,4 @@
+#Write a program that takes day and month from the command line and prints true if day of month is between March 20 and June 20, false otherwise.
 echo "Enter any date : "
 read d
 echo "Enter any month number from 1 to 12: "
